89cf283a36b9571a516ce4e8ef0dc1bf1822a47d
Mahesh mhaske <mahesh.mhaske0369@gmail.com>
2026-03-12 18:07:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -75,8 +75,8 @@ app.get("/",(req,res)=>{
 res.sendFile(path.join(__dirname,"index.html"))
 })
 
-const PORT=process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-console.log("Server running")
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`AgentHubs running on port ${PORT}`);
+});
